46758b1947c23d5363ddd985e76ca5d1ae23beaa
Marc Bungart <dev@webrails.de>
2012-10-28 01:39:50 +0200

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Webrails::Application.config.secret_token = '2c58aaa8fd80d52b7ad9368e5897b27dbb10d473f28894c20ca20f834c2368f9a68dd267962bcce57fc8aaf82d8e432255809ee2d075041e11fde0dce52febbf'
+Webrails::Application.config.secret_token = 'd0678f7efb39a036112864685a63df07a6b279e2348b806ecb09e5e76368059ed8c8c68550b4be65c56e6c58b9ea13145d288fa01d559acf634666747fb2b702'
